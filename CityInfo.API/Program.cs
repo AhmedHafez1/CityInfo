52eaa351c20efdda6622ab2aba4b2f0342ccd8b8
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-04-19 21:46:04 +0200

    mw

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -16,10 +16,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
+
+app.Run(async context => await context.Response.WriteAsync("Hello Wo"));
 
 app.Run();
