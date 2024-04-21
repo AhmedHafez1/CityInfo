f104aeec8eac2098819d3e705af5cc6de40ef465
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-04-21 08:59:57 +0200

    cities c

---
+++ CityInfo.API/Controllers/Cities.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                    new[]
+                    {
+                       new { id = 1, name = "Ghaza"  },
+                       new { id = 2, name = "Quds"  },
+                       new { id = 3, name = "Haifa"  },
+                       new { id = 4, name = "Khalil"  },
+                       new { id = 5, name = "Yafa"  }
+                    }
+                );
+        }
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -16,12 +16,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
-//app.MapControllers();
-
-app.Run(async context => await context.Response.WriteAsync("Hello Wo"));
+app.MapControllers();
 
 app.Run();
